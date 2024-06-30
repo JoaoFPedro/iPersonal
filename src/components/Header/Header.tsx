b67cfff60dcfd47bf3bb91b961ab96cb621ca74e
Joao Pedro <joao.fpsilva98@gmail.com>
2024-06-30 18:53:44 -0300

    feat: add avatar icon to header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,23 @@
 import { View } from "react-native";
-import WelcomePage from "../../pages/WelcomePage/WelcomePage";
+import { Avatar } from "@rneui/themed";
 import Button from "../Button/Button";
 import styles from "./styles";
 
-
 const Header = () => {
-    return ( 
-        <>
-        <View style={styles.container}>
-            <Button children="Feedback"/>
-            <Button children="Comentarios"/>
-            <Button children="Teste"/>
-            </View>
-            
-        </>
-     );
-}
- 
+  return (
+    <>
+      <View style={styles.container}>
+        <Avatar
+          size={48}
+          rounded
+          source={{ uri: "https://randomuser.me/api/portraits/men/36.jpg" }}
+        />
+        <Button children="Feedback" />
+        <Button children="Comentarios" />
+        <Button children="Teste" />
+      </View>
+    </>
+  );
+};
 
 export default Header;
