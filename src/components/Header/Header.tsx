76ed91743c8e65ab84b71bfddbcec36b6c0b9591
Joao Pedro <joao.fpsilva98@gmail.com>
2024-06-30 15:10:43 -0300

    feat: Adding styles to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,21 @@
+import { View } from "react-native";
+import WelcomePage from "../../pages/WelcomePage/WelcomePage";
 import Button from "../Button/Button";
+import styles from "./styles";
+
 
 const Header = () => {
     return ( 
         <>
+        <View style={styles.container}>
             <Button children="Feedback"/>
             <Button children="Comentarios"/>
+            <Button children="Teste"/>
+            </View>
+            
         </>
      );
 }
  
+
 export default Header;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,8 @@
+import { StyleSheet, View } from "react-native";
+const styles = StyleSheet.create({
+    container: {     
+      flexDirection: "row",
+    },
+  });
+
+  export default styles
